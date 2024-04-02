6809720e7a20553a9e4659f8a1db296b7c79e994
rachelleyia <165571005+rachelleyia@users.noreply.github.com>
2024-04-02 17:31:58 +0800

    Add files via upload

---
+++ Prog2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prog2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 3 == 0 && n % 5 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
